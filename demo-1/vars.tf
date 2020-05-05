86af90c09e93c006b5476c2339e0a5cd46e09f4e
Deepu1205 <anupamd8@gmail.com>
2020-05-05 08:48:48 +0000

    changing dem0-1,demo-2,first-steps

--- demo-1/vars.tf
+++ demo-1/vars.tf
@@ -1,9 +1,7 @@
 variable "AWS_ACCESS_KEY" {
 }
-
 variable "AWS_SECRET_KEY" {
 }
-
 variable "AWS_REGION" {
   default = "eu-west-1"
 }
@@ -11,9 +9,9 @@ variable "AWS_REGION" {
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
-    eu-west-1 = "ami-0d729a60"
+    ap-southeast-2 = "ami-09edd82f3e74e090d"
+    us-west-2 = "ami-003634241a8fcdec0"
+    eu-west-1 = "ami-08c8c70a9d43f0fdb"
   }
 }
 
--- first-steps/instance.tf
+++ first-steps/instance.tf
@@ -1,11 +1,12 @@
 provider "aws" {
-  access_key = "ACCESS_KEY_HERE"
-  secret_key = "SECRET_KEY_HERE"
-  region     = "us-east-1"
+  access_key = "AKIA4B3RKP5OXZ7TFM6N"
+  secret_key = "3JTeYeOcl6QiYhl3fF5b7XLRDgo9Ya0iqyC49QMk"
+  region     = "ap-southeast-2"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0d729a60"
+  ami           = "ami-04fcc97b5f6edcd89"
   instance_type = "t2.micro"
 }
+
 
